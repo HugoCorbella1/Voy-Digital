e395dea4df3668b11e5aac03098a7b4f04522d02
Hugo <hugocorbella140@gmail.com>
2021-09-05 12:39:58 -0500

    Barra de navegación

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,25 @@
+const secciones = document.querySelectorAll('section');
+
+const opciones = {
+    threshold: 0.5
+}
+
+let observador = new IntersectionObserver(verificadorDeNavegacion, opciones);
+
+function verificadorDeNavegacion(registros) {
+    registros.forEach(registro => {
+        const nombreDeLaClase = registro.target.className;
+        const anclajeActivo = document.querySelector(`[data-page = ${nombreDeLaClase}]`);
+        if(registro.isIntersecting) {
+            anclajeActivo.classList.add('seccionVisualizadaEnBarraDeNavegacion');
+        }
+
+        else {
+            anclajeActivo.classList.remove('seccionVisualizadaEnBarraDeNavegacion');
+        }
+    });
+}
+
+secciones.forEach(seccion => {
+    observador.observe(seccion);
+});
